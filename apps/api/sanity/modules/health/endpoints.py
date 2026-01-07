8e0849f1ddcf5bf72f4a2e4ded41ec55bce13a11
nednella <19449811+nednella@users.noreply.github.com>
2026-01-07 19:36:43 +0000

    Update /health endpoint to ping database

--- apps/api/sanity/modules/health/endpoints.py
+++ apps/api/sanity/modules/health/endpoints.py
@@ -1,8 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import select
+from sqlalchemy.exc import SQLAlchemyError
+
+from sanity.db.deps import AsyncSession, get_db_session
 
 router = APIRouter(prefix="/health")
 
 
 @router.get("")
-def health_check() -> dict[str, str]:
+async def health_check(
+    session: AsyncSession = Depends(get_db_session),
+) -> dict[str, str]:
+    try:
+        await session.execute(select(1))
+    except SQLAlchemyError as ex:
+        raise HTTPException(status_code=503, detail="Postgres database is not available") from ex
+
     return {"status": "OK"}
