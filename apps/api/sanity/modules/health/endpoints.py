47268d3d4c7d1a06e70f59d9919a00bd765f2287
nednella <119449811+nednella@users.noreply.github.com>
2026-01-07 20:17:11 +0000

    feat(api): replace MySQL with Postgres for bingo db (#10)
    * Install asyncpg
    
    * Uninstall asyncmy & cryptography
    
    * Replace MySQL image w/ Postgres image
    
    * Update /health endpoint to ping database
    
    * Add database session dependency type alias to reduce boilerplate
    
    ---------
    
    Co-authored-by: nednella <19449811+nednella@users.noreply.github.com>

--- apps/api/sanity/modules/health/endpoints.py
+++ apps/api/sanity/modules/health/endpoints.py
@@ -1,8 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from sqlalchemy import select
+
+from sanity.db.deps import DatabaseSession
 
 router = APIRouter(prefix="/health")
 
 
 @router.get("")
-def health_check() -> dict[str, str]:
+async def health_check(
+    session: DatabaseSession,
+) -> dict[str, str]:
+    try:
+        await session.execute(select(1))
+    except Exception as ex:
+        raise HTTPException(status_code=503, detail="Postgres database is not available") from ex
+
     return {"status": "OK"}
