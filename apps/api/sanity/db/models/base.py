dfd9b3cabd92b793b7f30387951d967db4b5b6c9
nednella <19449811+nednella@users.noreply.github.com>
2026-01-11 17:25:48 +0000

    Add db TimestampMixin model

--- apps/api/sanity/db/models/base.py
+++ apps/api/sanity/db/models/base.py
@@ -1,4 +1,8 @@
-from sqlalchemy.orm import DeclarativeBase
+from datetime import datetime
+from typing import Optional
+
+from sqlalchemy import DateTime, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
@@ -9,3 +13,28 @@ class Base(DeclarativeBase):
     """
 
     pass
+
+
+class TimestampMixin:
+    """
+    Mixin to add UTC auditing timestamps to DB models.
+
+    - `created_at` is set once on INSERT
+    - `updated_at` is automatically bumped on UPDATE
+    """
+
+    __abstract__ = True
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+        index=True,
+    )
+
+    updated_at: Mapped[Optional[datetime]] = mapped_column(
+        DateTime(timezone=True),
+        nullable=True,
+        default=None,
+        onupdate=func.now(),
+    )
