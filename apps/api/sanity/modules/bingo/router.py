bc570809f257b8a63f8509e334732393896f1de7
nednella <19449811+nednella@users.noreply.github.com>
2026-01-11 21:00:49 +0000

    Add bingo router skeleton

--- apps/api/sanity/modules/bingo/router.py
+++ apps/api/sanity/modules/bingo/router.py
@@ -1,3 +1,10 @@
 from fastapi import APIRouter
 
+from .board.router import router as board_router
+from .catalogue.router import router as catalogue_router
+from .event.router import router as event_router
+
 router = APIRouter(prefix="/bingo", tags=["Bingo"])
+router.include_router(board_router)
+router.include_router(catalogue_router)
+router.include_router(event_router)
