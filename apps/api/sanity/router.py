19cc2cc176bfdea865e973655a4f12888b6d2714
nednella <19449811+nednella@users.noreply.github.com>
2026-01-22 21:48:44 +0000

    Init bingo module

--- apps/api/sanity/router.py
+++ apps/api/sanity/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
+from sanity.modules.bingo.router import router as bingo_router
 from sanity.modules.health.router import router as health_router
 
 api_router = APIRouter(prefix="/v1")
+api_router.include_router(bingo_router)
 api_router.include_router(health_router)
