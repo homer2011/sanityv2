a157f78d8236523e3095d36fd455e206e4ec162b
nednella <19449811+nednella@users.noreply.github.com>
2026-01-22 22:14:06 +0000

    Add ItemRepository exists_by_boss_id_and_name method

--- apps/api/sanity/modules/bingo/item/repository.py
+++ apps/api/sanity/modules/bingo/item/repository.py
@@ -5,3 +5,13 @@ from .model import Item
 
 class ItemRepository(RepositoryBase[Item]):
     model = Item
+
+    async def exists_by_boss_id_and_name(self, boss_id: int, name: str) -> bool:
+        stmt = (
+            self.get_base_statement()
+            .where(self.model.boss_id == boss_id)
+            .where(self.model.name == name)
+        )
+
+        result = await self.get_one_or_none(stmt)
+        return result is not None
