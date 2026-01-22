ae26d630f5124adffd797e619f1f3e85807b72a9
nednella <19449811+nednella@users.noreply.github.com>
2026-01-22 22:13:42 +0000

    Add BossRepository exists_by_name method

--- apps/api/sanity/modules/bingo/boss/repository.py
+++ apps/api/sanity/modules/bingo/boss/repository.py
@@ -5,3 +5,9 @@ from .model import Boss
 
 class BossRepository(RepositoryBase[Boss]):
     model = Boss
+
+    async def exists_by_name(self, name: str) -> bool:
+        stmt = self.get_base_statement().where(self.model.name == name)
+
+        result = await self.get_one_or_none(stmt)
+        return result is not None
