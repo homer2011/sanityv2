c5d686a81fa5c69947bcfaf6ef772f5772acc427
nednella <19449811+nednella@users.noreply.github.com>
2026-01-22 19:35:16 +0000

    Implement changes to health endpoints

--- apps/api/sanity/modules/health/endpoints.py
+++ apps/api/sanity/modules/health/endpoints.py
@@ -1,18 +1,20 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 from sqlalchemy import select
 
 from sanity.db.deps import DatabaseSession
+from sanity.errors.exceptions import ServiceUnavailable
+from sanity.errors.schemas import ServiceUnavailableResponse
 
 router = APIRouter(prefix="/health")
 
 
-@router.get("")
+@router.get("", responses={503: ServiceUnavailableResponse})
 async def health_check(
     session: DatabaseSession,
 ) -> dict[str, str]:
     try:
         await session.execute(select(1))
     except Exception as ex:
-        raise HTTPException(status_code=503, detail="Postgres database is not available") from ex
+        raise ServiceUnavailable("Postgres database unavailable") from ex
 
     return {"status": "OK"}
