cbd750cbb7bde04f614f089f037615c04a3bc227
nednella <119449811+nednella@users.noreply.github.com>
2026-01-22 21:46:22 +0000

    feat(api): add separate read/write database session dependencies (#20)
    * Add separate read/write session dependencies for clarity
    
    * Implement dependency changes
    
    ---------
    
    Co-authored-by: nednella <19449811+nednella@users.noreply.github.com>

--- apps/api/sanity/db/deps.py
+++ apps/api/sanity/db/deps.py
@@ -2,15 +2,31 @@ from collections.abc import AsyncGenerator
 from typing import Annotated
 
 from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession as AsyncSession
 
 from sanity.db.core import async_session_factory
 
 
-async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
+async def get_db_read_session() -> AsyncGenerator[AsyncSession, None]:
     """
     Generates a new async db session scoped to a single request; consumed as a dependency
     within a service layer.
+
+    Intended to be used in a read-only manner, and does not auto-commit.
+
+    docs: https://docs.sqlalchemy.org/en/20/orm/session_api.html#sqlalchemy.orm.sessionmaker
+    """
+
+    async with async_session_factory() as session:
+        yield session
+
+
+async def get_db_write_session() -> AsyncGenerator[AsyncSession, None]:
+    """
+    Generates a new async db session scoped to a single request; consumed as a dependency
+    within a service layer.
+
+    Intended to be used in a read-write manner, and includes auto-commit.
 
     docs: https://docs.sqlalchemy.org/en/20/orm/session_api.html#sqlalchemy.orm.sessionmaker
     """
@@ -18,13 +34,12 @@ async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_factory() as session:
         try:
             yield session
+            await session.commit()
 
         except Exception:
             await session.rollback()  # a'la Spring Boot @Transactional
             raise
 
-        finally:
-            await session.close()
 
-
-DatabaseSession = Annotated[AsyncSession, Depends(get_db_session)]  # type alias
+DatabaseReadSession = Annotated[AsyncSession, Depends(get_db_read_session)]  # type alias
+DatabaseWriteSession = Annotated[AsyncSession, Depends(get_db_write_session)]  # type alias
