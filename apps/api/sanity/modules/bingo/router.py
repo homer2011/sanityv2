ad3c4071f735f7b1c17ab925a0271f3dbf754bc6
nednella <19449811+nednella@users.noreply.github.com>
2026-01-24 21:59:45 +0000

    init bingo board & tile features

--- apps/api/sanity/modules/bingo/router.py
+++ apps/api/sanity/modules/bingo/router.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter
 
+from .board.router import router as board_router
 from .catalogue.router import router as catalogue_router
 from .event.router import router as event_router
+from .tile.router import router as tile_router
 
 router = APIRouter(prefix="/bingo")
+router.include_router(board_router)
 router.include_router(catalogue_router)
 router.include_router(event_router)
+router.include_router(tile_router)
