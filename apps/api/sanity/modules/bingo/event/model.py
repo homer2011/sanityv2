070b9829ab79a88fe6961c07747c5fec85f39a8d
nednella <19449811+nednella@users.noreply.github.com>
2026-01-24 18:53:40 +0000

    Add initial Event model

---
+++ apps/api/sanity/modules/bingo/event/model.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+from enum import StrEnum
+
+from apps.api.sanity.common.utils import utc_now
+from sqlalchemy import DateTime, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from sanity.db.models import RecordModel
+
+
+# TODO: consider persisted lifecycle
+# TODO: consider soft-delete
+class Event(RecordModel):
+    class Status(StrEnum):
+        DRAFT = "DRAFT"
+        SCHEDULED = "SCHEDULED"
+        LIVE = "LIVE"
+        ENDED = "ENDED"
+
+    __tablename__ = "events"
+
+    title: Mapped[str] = mapped_column(
+        String(255),
+        nullable=False,
+    )
+
+    starts_at: Mapped[datetime | None] = mapped_column(
+        DateTime(timezone=True),
+        nullable=True,
+        default=None,
+        index=True,
+    )
+
+    ends_at: Mapped[datetime | None] = mapped_column(
+        DateTime(timezone=True),
+        nullable=True,
+        default=None,
+        index=True,
+    )
+
+    @property
+    def status(self) -> Status:
+        if self.starts_at is None or self.ends_at is None:
+            return self.Status.DRAFT
+
+        now = utc_now()
+        if now < self.starts_at:
+            return self.Status.SCHEDULED
+
+        if now < self.ends_at:
+            return self.Status.LIVE
+
+        return self.Status.ENDED
